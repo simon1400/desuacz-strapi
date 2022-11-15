654b3e2bdf8fdd5685fc546f5feb11f5bbf2a611
Your Name <>
2022-11-15 17:27:18 +0100

    sdhgsfd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       user : 'dimi',
       host : ['89.221.216.23'],
       ref  : 'origin/main',
-      repo : 'git@github.com:simon1400/kp-demo-strapi.git',
+      repo : 'git@github.com:simon1400/desuacz-strapi.git',
       path : '/var/www/desuacz/strapi',
       'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
     }
