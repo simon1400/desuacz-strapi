c72c1533ac90ad589df66618400af47c34644458
Your Name <>
2022-11-23 23:21:58 +0100

    add plugins settings

---
+++ config/plugins.js
@@ -0,0 +1,32 @@
+module.exports = {
+  meilisearch: {
+    config: {
+      product: {
+        transformEntry({ entry }) {
+          return {
+            ...entry,
+            categoryTitles: entry.category.map(item => item.title),
+          }
+        },
+        settings: {
+          filterableAttributes: ['categoryTitles'],
+          sortableAttributes: ['price'],
+          searchableAttributes: [
+            'title',
+            'categoryTitles',
+            "price",
+            'slug'
+          ],
+        },
+      }
+    }
+  },
+  graphql: {
+    enabled: true,
+    config: {
+      defaultLimit: 50,
+      depthLimit: 50,
+      maxLimit: 20,
+    }
+  }
+}
