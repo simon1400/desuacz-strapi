0c206a0719c3d71e7fdf26002f7c002dca5c7436
Your Name <>
2023-04-16 10:47:34 +0200

    add postgres for prod and add cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -24,6 +24,20 @@ module.exports = {
       }
     }
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
   graphql: {
     enabled: true,
     config: {
