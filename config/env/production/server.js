3cefc55ba5628b58c9fac405004921f98aeaa99e
Your Name <>
2023-04-16 11:10:21 +0200

    add server new

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1446),
   url: env('HOST_URL'), // Sets the public URL of the application.
   app: {
     keys: env.array('APP_KEYS')
